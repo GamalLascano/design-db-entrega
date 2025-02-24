ec577629faff50a73ba3228683caab8df7cbe520
GamalLascano <gamallascano@gmail.com>
2025-02-24 00:31:21 -0300

    Primer commit

---
+++ src/main/java/com/unlp/proyecto_db_design/ProyectoDbDesignApplication.java
@@ -0,0 +1,13 @@
+package com.unlp.proyecto_db_design;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoDbDesignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoDbDesignApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unlp/proyecto_db_design/ProyectoDbDesignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unlp.proyecto_db_design;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoDbDesignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
